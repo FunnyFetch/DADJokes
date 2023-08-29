8141f647facebd3c6ad4f1a578777c638b198a94
SirreaReeves <reevessirrea@gmail.com>
2023-08-29 11:34:31 -0400

    Updated

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const apiURL = 'https://official-joke-api.appspot.com/jokes/random';
 const random = async () => {
   const res = await fetch(apiURL)
   const joke = await res.json();
-  console.log('joke:', joke)
-}
+  console.log('joke:', joke);
+};
 
-random()
+random();
