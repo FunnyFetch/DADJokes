16cabc365ba2fc503451bb81fecd2c0bf39c8d38
SirreaReeves <reevessirrea@gmail.com>
2023-08-30 00:24:42 -0400

    Semantics updated

--- script.js
+++ script.js
@@ -12,16 +12,20 @@ const random = async () => {
   const res = await fetch(apiURL)
   const joke = await res.json();
   console.log('joke:', joke);
+  
   const p = document.createElement("p");
   const p2 = document.createElement("p2");
   p.textContent = joke.setup;
   p2.textContent = joke.punchline;
-  document.body.appendChild(p);
-  document.body.appendChild(p2);
+  
+  const jokeContainer = document.querySelector('#generator');
+  jokeContainer.appendChild(p);
+  jokeContainer.appendChild(p2)
+ 
   const button = document.createElement("button");
   button.textContent = "CLICK ME!";
   button.id = "button";
-  document.body.appendChild(button);
+  jokeContainer.appendChild(button);
   
   const getRandomJoke = async () => {
       const res = await fetch(apiURL, {
