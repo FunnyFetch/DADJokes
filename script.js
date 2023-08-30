32535bfff8f068a37ba210ef693a9f47869e1d1f
SirreaReeves <reevessirrea@gmail.com>
2023-08-30 00:20:08 -0400

    Random joke onclick updated

--- script.js
+++ script.js
@@ -14,10 +14,28 @@ const random = async () => {
   console.log('joke:', joke);
   const p = document.createElement("p");
   const p2 = document.createElement("p2");
-  p.textContent = joke.setup
-  p2.textContent = joke.punchline
-  document.body.appendChild(p)
-  document.body.appendChild(p2)
+  p.textContent = joke.setup;
+  p2.textContent = joke.punchline;
+  document.body.appendChild(p);
+  document.body.appendChild(p2);
+  const button = document.createElement("button");
+  button.textContent = "CLICK ME!";
+  button.id = "button";
+  document.body.appendChild(button);
+  
+  const getRandomJoke = async () => {
+      const res = await fetch(apiURL, {
+          headers: {
+              'Accept': 'application/json'
+            }
+        });
+        const joke = await res.json();
+        console.log(joke);
+        p.innerHTML = joke.setup
+        p2.innerHTML = joke.punchline;
+
+    }
+    button.addEventListener("click", getRandomJoke);
 };
 
 tenJokes();
